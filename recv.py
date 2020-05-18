fc8ccca065169792cfd8ee9ff0a23d6f8cd6320d
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-18 15:59:52 +0530

    Streamlined file reception over IPv6

--- recv.py
+++ recv.py
@@ -2,17 +2,12 @@ from flask import Flask, request
 
 main = Flask(__name__)
 
-@main.route("/recvarch/", methods=["GET"])
-def sendarch():
-    if request.method == "GET":
-        jsondata = request.get_json()
-        filename = jsondata["filename"]
-        actifile = jsondata["actifile"]
-        print(filename, len(actifile))
-        retndata = {
-            "notecode": "File " + filename + " has been successfully received!",
-        }
-        return retndata
+@main.route("/recvarch/", methods=["POST"])
+def recvarch():
+    if request.method == "POST":
+        f = request.files["files"]
+        print(f)
+        return "Successful"
 
 if __name__ == "__main__":
-    main.run(port=9696, host="0.0.0.0")
+    main.run(port=9696, host="::")
