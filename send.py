0bfabdc932c159512393cb76963df52391ff014a
t0xic0der <akashdeep.dhar@gmail.com>
2020-05-18 16:00:25 +0530

    Streamlined file transmission over IPv6

--- send.py
+++ send.py
@@ -1,12 +1,14 @@
 import requests
 
-targetpc = "localhost:9696"
+targetpc = "http://localhost:9696/recvarch/"
 
-def sendfile(filename):
-    actifile = open(filename,"rb")
-    data = {
-        "filename": filename,
-        "actifile": actifile,
-    }
-    rqst = requests.get(targetpc, json=data)
-    print(rqst.json())
+targetpc = "http://[2409:4042:2396:2f42:9387:22e4:aa39:af0e]:9696/recvarch/"
+
+def sendarch(filename):
+    files = {'files': open(filename, 'rb')}
+    r = requests.post(targetpc, files=files)
+    print(r.text)
+
+if __name__ == "__main__":
+    filename = str(input("Enter the filename "))
+    sendarch(filename)
