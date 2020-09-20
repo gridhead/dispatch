c4aa6364793e96643eb1064c4817b8ac360aa524
t0xic0der <akashdeep.dhar@gmail.com>
2020-09-20 23:03:45 +0530

    Added integrity and identity verification
    At both sender's and receiver's end

--- file/recv.py
+++ file/recv.py
@@ -1,16 +1,40 @@
 from flask import Flask, request
+import requests, hashlib, os, logging
 
 main = Flask(__name__)
+loge = logging.getLogger("werkzeug")
+loge.disabled = True
 
 @main.route("/recvarch/", methods=["POST"])
 def recvarch():
     if request.method == "POST":
         f = request.files["files"]
-        print(f)
-        respjson = {
-            "respcode": "donesend"
-        }
-        return respjson
+        print(" * Transfer buffer cached to storage")
+        f.save(f.filename+".bak")
+        return {"respcode": "donesend"}
+
+
+@main.route("/connsend/", methods=["GET"])
+def connsend():
+    if request.method == "GET":
+        print(" * Identity proven to a sender")
+        return {"hash": hashlib.sha512(request.get_json()["password"].encode("utf8")).hexdigest()}
+
+
+@main.route("/filechek/", methods=["GET"])
+def filechek():
+    try:
+        if request.method == "GET":
+            if hashlib.sha512(open(request.get_json()["name"], "rb").read()).hexdigest() == request.get_json()["hash"]:
+                os.system("mv " + request.get_json()["name"] + " " + request.get_json()["name"][:-4])
+                print(" * Cache integrity verified"+ "\n" + " * Transfer complete")
+                return {"respcode": "verified"}
+            os.system("rm " + request.get_json()["name"])
+            return {"respcode": "verifail"}
+    except Exception as expt:
+        return {"respcode": str(expt)}
+
 
 if __name__ == "__main__":
+    print(" * IPv6 address is", requests.get("https://ipv6.icanhazip.com/").text, end="")
     main.run(port=9696, host="::")
--- file/send.py
+++ file/send.py
@@ -1,14 +1,47 @@
-import requests
+import requests, hashlib
 
 targetpc = "http://localhost:9696/recvarch/"
 
 targetpc = "http://[2409:4042:2396:2f42:9387:22e4:aa39:af0e]:9696/recvarch/"
 
-def sendarch(filename):
-    files = {'files': open(filename, 'rb')}
-    r = requests.post(targetpc, files=files)
-    print(r.json)
+def sendhash(filename, targetpc):
+    try:
+        hash = hashlib.sha512(open(filename, "rb").read()).hexdigest()
+        objc = requests.get("http://[" + targetpc + "]:9696/filechek/", json={"name": filename + ".bak", "hash": hash})
+        if objc.status_code == 200:
+            if objc.json()["respcode"] == "verified":
+                print("[3/3] File integrity verified at receiver's end!")
+            else:
+                print("Integrity verification failed")
+        else:
+            print("Receiver is facing errors")
+    except Exception as expt:
+        print("Exception " + str(expt) + " has taken place")
+
+
+def sendarch(filename, targetpc):
+    file = {"files": open(filename, "rb")}
+    r = requests.post("http://[" + targetpc + "]:9696/recvarch/", files=file)
+    if r.json()["respcode"] == "donesend":
+        print("[2/3] File transfer complete! Sending file hash...")
+        sendhash(filename, targetpc)
+
+
+def connrecv(filename, targetpc):
+    try:
+        safetext = "password"
+        objc = requests.get("http://[" + targetpc + "]:9696/connsend/", json={"password": safetext})
+        if objc.status_code == 200:
+            if objc.json()["hash"] == hashlib.sha512(safetext.encode("utf8")).hexdigest():
+                print("[1/3] Receiver legitimacy verified! Transferring file...")
+                sendarch(filename, targetpc)
+            else:
+                print("Verification failed")
+        else:
+            print("Receiver is facing errors")
+    except Exception as expt:
+        print("Exception " + str(expt) + " has taken place")
+
 
 if __name__ == "__main__":
-    filename = str(input("Enter the filename "))
-    sendarch(filename)
+    connrecv(str(input("Enter the filename ")), str(input("Enter the destination PC address ")))
